2cc07e78be15c8245c58b49c77d6b72370eeb566
demiandk <152564707+demiandk@users.noreply.github.com>
2026-05-08 08:45:48 -0400

    Readjusting for my digital portfolio and adding more css and starting on js functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+//html elements
+const mainPage = getElementById("mainPage");
+
+
+//functions
+
+//function that changes the pages and the header content t let the user know which page they are on
+function switchPage(page){
+    page.style.dsplay = "block";
+}
