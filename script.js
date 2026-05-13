1d72ec5c1579bb5b25132d574a77d589d18cf574
demiandk <152564707+demiandk@users.noreply.github.com>
2026-05-13 10:33:00 -0400

    stuff

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const mainPage = getElementById("mainPage");
 //functions
 
 //function that changes the pages and the header content to let the user know which page they are on
-function switchPage(page){
+/*function switchPage(page){
     page.style.dsplay = "block";
 }
+*/
