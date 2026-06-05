8943615ffa3f6fd420b5646f78d1b160c6770ab0
demiandk <152564707+demiandk@users.noreply.github.com>
2026-06-05 00:12:44 -0400

    Made shortcuts to meet the e-portfolio deadline

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ const mainPage = getElementById("mainPage");
 
 //functions
 
-//function that changes the pages and the header content to let the user know which page they are on
-/*function switchPage(page){
-    page.style.dsplay = "block";
-}
-*/
